b47746333065a853b9d0d7d8868d01f4fb8a13f6
mozta <perezaguirrerafael@gmail.com>
2024-10-23 16:17:32 -0600

    source code

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Mi primer backend"
+
+@app.route('/saludos')
+def saludos():
+    return "Que onda!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
